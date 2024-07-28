0781ce8a7c9e2ad79cb6dd4d032d3776ee7a47b0
Oleksii S <stokrockyioleksii@gmail.com>
2024-07-28 13:58:32 +0300

    fix: linter

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const js = require("@eslint/js");
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const prettierConfig = require("eslint-config-prettier");
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+        },
+    },
+    prettierConfig,
+];
