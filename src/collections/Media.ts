04b24004b479bccb856b85f042637c18f209d545
Oleksii S <stokrockyioleksii@gmail.com>
2024-07-28 19:02:27 +0300

    fix: media route

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -6,7 +6,7 @@ import { admins } from '../access/admins';
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
-    staticDir: path.resolve(__dirname, '../../../media'),
+    staticDir: path.resolve(__dirname, '../../media'),
   },
   access: {
     read: adminsOrPublished,
